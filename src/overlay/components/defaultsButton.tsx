6de2490a99087dce090786c11c78cfc2b018533a
Tomáš Sabo <tomsk.slovak@gmail.com>
2025-07-20 23:48:04 +0200

    button to load default settings

--- src/overlay/components/defaultsButton.tsx
+++ src/overlay/components/defaultsButton.tsx
@@ -1,40 +1,55 @@
 import { useState } from "react";
 
-export const ReloadButton = () => {
+import type { Config } from "../../shared/types";
+import { defaultConfig } from "../../shared/types";
+
+type DefaultsButtonProps = {
+    config: Config;
+    setConfig: React.Dispatch<React.SetStateAction<Config>>;
+};
+
+export const DefaultsButton: React.FC<DefaultsButtonProps> = ({
+    config,
+    setConfig,
+}) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleConfirm = () => {
         setIsOpen(false);
-        window.electronAPI.reloadGFN();
+        setConfig(defaultConfig);
     };
 
     return (
         <div className="flex">
             <button
                 onClick={() => setIsOpen(true)}
-                className="bg-blue-600 hover:bg-blue-700 w-48 text-white text-lg font-semibold py-4 px-6 rounded shadow"
+                className="bg-gray-600 hover:bg-gray-700 w-48 text-white text-lg font-semibold py-4 px-4 rounded shadow"
             >
-                Reload GFN
+                Defaults Settings
             </button>
 
             {isOpen && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                     <div className="bg-[#0c1015] text-[#babec4] rounded-lg p-6 max-w-sm w-full shadow-xl text-center border border-gray-600">
                         <h2 className="text-lg mb-4">
-                            This action will kick you out of the running game.
+                            Load defaults settings?
+                            <br />
+                            <small>
+                                Restart application to apply all changes
+                            </small>
                         </h2>
                         <div className="flex justify-center space-x-4 py-4">
                             <button
                                 onClick={handleConfirm}
                                 className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
                             >
-                                OK
+                                Yes
                             </button>
                             <button
                                 onClick={() => setIsOpen(false)}
                                 className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded"
                             >
-                                Cancel
+                                No
                             </button>
                         </div>
                     </div>
