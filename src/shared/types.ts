841cd9df5c04b86593a66cade999815fde05bcfb
Tomáš Sabo <tomsk.slovak@gmail.com>
2025-07-28 08:19:14 +0200

    added automute

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,6 +1,7 @@
 export const defaultConfig: Config = {
     userAgent: "",
     autofocus: false,
+    automute: false,
     notify: true,
     rpcEnabled: true,
     informed: false,
@@ -10,6 +11,7 @@ export const defaultConfig: Config = {
 export interface Config {
     userAgent: string;
     autofocus: boolean;
+    automute: boolean;
     notify: boolean;
     rpcEnabled: boolean;
     informed: boolean;
